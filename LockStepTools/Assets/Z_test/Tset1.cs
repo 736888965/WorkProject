bf45711121e18fa77dd7b8dd24d26f56be5aa389
736888965 <736888965@qq.com>
2020-04-03 17:43:11 +0800

    add  添加内容

--- LockStepTools/Assets/Z_test/Tset1.cs
+++ LockStepTools/Assets/Z_test/Tset1.cs
@@ -7,7 +7,7 @@ public class Tset1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("%%%%%%%%%%%%%");
     }
 
     // Update is called once per frame
